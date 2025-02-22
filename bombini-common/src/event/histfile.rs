71032e2f726796df7e2d2cb41bc2d2c701f3bad3
Andrei Fedotov <splashgitar@gmail.com>
2025-02-22 12:14:55 +0300

    Add process information to histfile detector

--- bombini-common/src/event/histfile.rs
+++ bombini-common/src/event/histfile.rs
@@ -1,11 +1,15 @@
 //! Histfile event module
 
+use crate::event::process::ProcInfo;
+
 pub const MAX_BASH_COMMAND_SIZE: usize = 128;
 
 /// Histfile execution event
 #[derive(Clone, Debug)]
 #[repr(C)]
 pub struct HistFileMsg {
+    /// bash proc info
+    pub process: ProcInfo,
     /// bash read line
     pub command: [u8; MAX_BASH_COMMAND_SIZE],
 }
--- bombini-detectors-ebpf/src/bin/histfile/main.rs
+++ bombini-detectors-ebpf/src/bin/histfile/main.rs
@@ -2,26 +2,38 @@
 #![no_main]
 
 use aya_ebpf::{
-    helpers::bpf_probe_read_user_str_bytes,
+    helpers::{bpf_get_current_pid_tgid, bpf_probe_read_user_str_bytes},
     macros::{map, uretprobe},
-    maps::lpm_trie::{Key, LpmTrie},
+    maps::{
+        hash_map::HashMap,
+        lpm_trie::{Key, LpmTrie},
+    },
     programs::retprobe::RetProbeContext,
 };
 
 use bombini_common::event::histfile::MAX_BASH_COMMAND_SIZE;
+use bombini_common::event::process::ProcInfo;
 use bombini_common::event::{Event, MSG_HISTFILE};
-use bombini_detectors_ebpf::{event_capture, event_map::rb_event_init};
+use bombini_detectors_ebpf::{event_capture, event_map::rb_event_init, util};
 
 #[map]
 static HIST_CHECK_MAP: LpmTrie<[u8; MAX_BASH_COMMAND_SIZE], u32> = LpmTrie::with_max_entries(2, 0);
+
+#[map]
+static PROC_MAP: HashMap<u32, ProcInfo> = HashMap::pinned(1024, 0);
 
 #[uretprobe]
 pub fn histfile_detect(ctx: RetProbeContext) -> i32 {
     event_capture!(ctx, MSG_HISTFILE, try_detect, true) as i32
 }
 
-fn try_detect(ctx: RetProbeContext, event: &mut Event, _expose: bool) -> Result<u32, u32> {
+fn try_detect(ctx: RetProbeContext, event: &mut Event, expose: bool) -> Result<u32, u32> {
     let Event::HistFile(event) = event else {
+        return Err(0);
+    };
+    let pid = (bpf_get_current_pid_tgid() >> 32) as u32;
+    let proc = unsafe { PROC_MAP.get(&pid) };
+    let Some(proc) = proc else {
         return Err(0);
     };
     let command: *const u8 = ctx.ret().ok_or(0u32)?;
@@ -31,6 +43,10 @@ fn try_detect(ctx: RetProbeContext, event: &mut Event, _expose: bool) -> Result<
     }
     let lookup = Key::new((MAX_BASH_COMMAND_SIZE * 8) as u32, event.command);
     if HIST_CHECK_MAP.get(&lookup).is_some() {
+        // Copy process info to Rb
+        if expose {
+            util::copy_proc(proc, &mut event.process);
+        }
         Ok(0)
     } else {
         Err(0)
