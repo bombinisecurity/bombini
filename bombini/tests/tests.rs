5b3b099fca7e867c06b12fc42c1eb50aab6f1a11
Andrei Fedotov <splashgitar@gmail.com>
2025-03-02 17:44:39 +0300

    test: add test_gtfobins_detector_file
    Add test to check gtfobins detector

--- bombini/tests/tests.rs
+++ bombini/tests/tests.rs
@@ -63,10 +63,6 @@ fn test_detectors_load() {
 }
 
 #[test]
-#[ignore]
-fn test_gtfobins_detector_file() {}
-
-#[test]
 fn test_procmon_file() {
     let mut project_dir = PathBuf::from(PROJECT_DIR);
     project_dir.pop();
@@ -136,6 +132,94 @@ fn test_procmon_file() {
     let events = fs::read_to_string(&event_log).expect("can't read events");
     assert_eq!(events.matches("\"filename\":\"ls\"").count(), 2);
     assert_eq!(events.matches("\"args\":\"-lah\"").count(), 2);
+
+    let _ = fs::remove_dir(bomini_temp_dir);
+}
+
+#[test]
+fn test_gtfobins_detector_file() {
+    let mut project_dir = PathBuf::from(PROJECT_DIR);
+    project_dir.pop();
+    let mut config = project_dir.clone();
+    config.push("config/config.yaml");
+    let mut bpf_objs = project_dir.clone();
+    bpf_objs.push("target/bpfel-unknown-none");
+    if EXE_BOMBINI.contains("release") {
+        bpf_objs.push("release");
+    } else {
+        bpf_objs.push("debug");
+    }
+
+    let temp_dir = Builder::new()
+        .prefix("bombini-test-")
+        .rand_bytes(5)
+        .keep(true)
+        .tempdir()
+        .expect("can't create temp dir");
+
+    let bomini_temp_dir = temp_dir.path();
+    let mut tmp_config = bomini_temp_dir.join("config/config.yaml");
+    let _ = fs::create_dir(bomini_temp_dir.join("config"));
+    let _ = fs::copy(&config, &tmp_config);
+    tmp_config.pop();
+    config.pop();
+    let _ = fs::copy(config.join("procmon.yaml"), tmp_config.join("procmon.yaml"));
+    let _ = fs::copy(
+        config.join("gtfobins.yaml"),
+        tmp_config.join("gtfobins.yaml"),
+    );
+    let bombini_log =
+        File::create(bomini_temp_dir.join("bombini.log")).expect("can't create log file");
+    let event_log = temp_dir.path().join("events.log");
+
+    let bombini = Command::new(EXE_BOMBINI)
+        .args([
+            "--config-dir",
+            tmp_config.to_str().unwrap(),
+            "--bpf-objs",
+            bpf_objs.to_str().unwrap(),
+            "--event-log",
+            event_log.to_str().unwrap(),
+            "--detector",
+            "procmon",
+            "--detector",
+            "gtfobins",
+        ])
+        .env("RUST_LOG", "debug")
+        .stderr(bombini_log.try_clone().unwrap())
+        .spawn();
+
+    if bombini.is_err() {
+        panic!("{:?}", bombini.err().unwrap());
+    }
+    let mut bombini = bombini.expect("failed to start bombini");
+    // Wait for detectors being loaded
+    thread::sleep(Duration::from_millis(1500));
+
+    let mut gtfo_proc = Command::new("sudo")
+        .args(["xargs", "-a", "/dev/null", "sh"])
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .stdin(Stdio::null())
+        .spawn()
+        .expect("can't start ls");
+
+    // Wait Events being processed
+    thread::sleep(Duration::from_millis(1000));
+
+    let _ = signal::kill(Pid::from_raw(gtfo_proc.id() as i32), Signal::SIGKILL);
+
+    let _ = gtfo_proc.wait().unwrap();
+
+    let _ = signal::kill(Pid::from_raw(bombini.id() as i32), Signal::SIGINT);
+
+    let _ = bombini.wait().unwrap();
+
+    // TODO: more precise check
+    let events = fs::read_to_string(&event_log).expect("can't read events");
+    assert_eq!(events.matches("\"type\":\"GTFOBinsEvent\"").count(), 1);
+    assert_eq!(events.matches("\"filename\":\"xargs\"").count(), 1);
+    assert_eq!(events.matches("\"args\":\"-a /dev/null sh\"").count(), 1);
 
     let _ = fs::remove_dir(bomini_temp_dir);
 }
