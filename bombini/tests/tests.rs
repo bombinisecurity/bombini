e17f8cab9e77e721d7804b6bf3677f9d1a049cc2
Andrei Fedotov <splashgitar@gmail.com>
2025-05-27 22:17:09 +0300

    tests: add test_netmon_tcp_ip4_file

--- bombini/tests/tests.rs
+++ bombini/tests/tests.rs
@@ -281,3 +281,89 @@ fn test_filemon_unlink_file() {
 
     let _ = fs::remove_dir_all(bombini_temp_dir);
 }
+
+#[test]
+#[ignore = "Github CI runners doesn't support bpf LSM. TODO: Run tests in VM"]
+fn test_netmon_tcp_ip4_file() {
+    let (temp_dir, mut config, bpf_objs) = init_test_env();
+    let bombini_temp_dir = temp_dir.path();
+    let mut tmp_config = bombini_temp_dir.join("config/config.yaml");
+    let _ = fs::create_dir(bombini_temp_dir.join("config"));
+    let _ = fs::copy(&config, &tmp_config);
+    tmp_config.pop();
+    config.pop();
+    let _ = fs::copy(config.join("procmon.yaml"), tmp_config.join("procmon.yaml"));
+    let _ = fs::copy(config.join("netmon.yaml"), tmp_config.join("netmon.yaml"));
+    let bombini_log =
+        File::create(bombini_temp_dir.join("bombini.log")).expect("can't create log file");
+    let event_log = temp_dir.path().join("events.log");
+
+    let bombini = Command::new(EXE_BOMBINI)
+        .args([
+            "--config-dir",
+            tmp_config.to_str().unwrap(),
+            "--bpf-objs",
+            bpf_objs.to_str().unwrap(),
+            "--event-log",
+            event_log.to_str().unwrap(),
+            "--detector",
+            "procmon",
+            "--detector",
+            "netmon",
+        ])
+        .env("RUST_LOG", "debug")
+        .stderr(bombini_log.try_clone().unwrap())
+        .spawn();
+
+    if bombini.is_err() {
+        panic!("{:?}", bombini.err().unwrap());
+    }
+    let mut bombini = bombini.expect("failed to start bombini");
+    // Wait for detectors being loaded
+    thread::sleep(Duration::from_millis(1500));
+
+    let mut nc = Command::new("nc")
+        .args(["-l", "7878"])
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .stdin(Stdio::null())
+        .spawn()
+        .expect("can't start nc");
+
+    // Wait nc
+    thread::sleep(Duration::from_millis(500));
+
+    let _ = Command::new("telnet")
+        .args(["localhost", "7878"])
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .stdin(Stdio::null())
+        .spawn()
+        .expect("can't start nc");
+
+    // Wait Events being processed
+    thread::sleep(Duration::from_millis(500));
+
+    let _ = signal::kill(Pid::from_raw(nc.id() as i32), Signal::SIGKILL);
+
+    let _ = nc.wait().unwrap();
+
+    let _ = signal::kill(Pid::from_raw(bombini.id() as i32), Signal::SIGINT);
+
+    let _ = bombini.wait().unwrap();
+
+    // TODO: more precise check
+    let events = fs::read_to_string(&event_log).expect("can't read events");
+    // inet_csk_accept isn't triggered from tests don't know why
+    ma::assert_ge!(events.matches("\"type\":\"NetworkEvent\"").count(), 2);
+    assert_eq!(
+        events
+            .matches("\"type\":\"TcpConnectionEstablish\"")
+            .count(),
+        1
+    );
+    ma::assert_ge!(events.matches("\"type\":\"TcpConnectionClose\"").count(), 1);
+    assert_eq!(events.matches("\"args\":\"localhost 7878\"").count(), 2);
+
+    let _ = fs::remove_dir_all(bombini_temp_dir);
+}
