c081aca445e3b0003e69e86dec0f9f40119e7934
Andrei Fedotov <splashgitar@gmail.com>
2025-06-15 19:08:37 +0300

    refactor: increase MAX_FILE_PATH

--- bombini-common/src/constants.rs
+++ bombini-common/src/constants.rs
@@ -4,6 +4,6 @@ pub const MAX_FILENAME_SIZE: usize = 32;
 
 pub const MAX_ARGS_SIZE: usize = 1024;
 
-pub const MAX_FILE_PATH: usize = 512;
+pub const MAX_FILE_PATH: usize = 1024;
 
 pub const MAX_FILE_PREFIX: usize = 256;
