00d8d36b777bdd6c173a25bca1766af6fcc7b353
Andrei Fedotov <splashgitar@gmail.com>
2025-06-21 18:29:00 +0300

    test: add test_iouring_allow_list_file

--- bombini/tests/tests.rs
+++ bombini/tests/tests.rs
@@ -387,8 +387,6 @@ process_allow_list:
     - 0
   euid:
     - 0
-  auid:
-    - 1000
   binary:
     name:
       - tail
@@ -536,4 +534,81 @@ process_deny_list:
     assert_eq!(events.matches("\"args\":\"-lah\"").count(), 2);
 
     let _ = fs::remove_dir_all(bombini_temp_dir);
+}
+
+#[test]
+#[ignore = "Github CI runners doesn't support bpf LSM. TODO: Run tests in VM"]
+fn test_iouring_allow_list_file() {
+    let (temp_dir, mut config, bpf_objs) = init_test_env();
+    let bombini_temp_dir = temp_dir.path();
+    let mut tmp_config = bombini_temp_dir.join("config/config.yaml");
+    let _ = fs::create_dir(bombini_temp_dir.join("config"));
+    let _ = fs::copy(&config, &tmp_config);
+    tmp_config.pop();
+    config.pop();
+    let _ = fs::copy(config.join("procmon.yaml"), tmp_config.join("procmon.yaml"));
+    let bombini_log =
+        File::create(bombini_temp_dir.join("bombini.log")).expect("can't create log file");
+    let config_contents = r#"
+expose-events: true
+process_allow_list:
+  uid:
+    - 0
+  euid:
+    - 0
+  binary:
+    name:
+      - nslookup
+"#;
+    let _ = fs::write(tmp_config.join("io_uringmon.yaml"), config_contents);
+    let event_log = temp_dir.path().join("events.log");
+
+    let bombini = Command::new(EXE_BOMBINI)
+        .args([
+            "--config-dir",
+            tmp_config.to_str().unwrap(),
+            "--bpf-objs",
+            bpf_objs.to_str().unwrap(),
+            "--event-log",
+            event_log.to_str().unwrap(),
+            "--detector",
+            "procmon",
+            "--detector",
+            "io_uringmon",
+        ])
+        .env("RUST_LOG", "debug")
+        .stderr(bombini_log.try_clone().unwrap())
+        .spawn();
+
+    if bombini.is_err() {
+        panic!("{:?}", bombini.err().unwrap());
+    }
+    let mut bombini = bombini.expect("failed to start bombini");
+    // Wait for detectors being loaded
+    thread::sleep(Duration::from_millis(1500));
+
+    let _ = Command::new("nslookup")
+        .args(["google.com"])
+        .stdout(Stdio::null())
+        .status()
+        .expect("can't start nslookup");
+
+    // Wait Events being processed
+    thread::sleep(Duration::from_millis(500));
+
+    let _ = signal::kill(Pid::from_raw(bombini.id() as i32), Signal::SIGINT);
+
+    let _ = bombini.wait().unwrap();
+
+    // TODO: more precise check
+    let events = fs::read_to_string(&event_log).expect("can't read events");
+    ma::assert_ge!(events.matches("\"filename\":\"nslookup\"").count(), 2);
+    ma::assert_ge!(events.matches("\"args\":\"google.com\"").count(), 2);
+    ma::assert_ge!(events.matches("\"type\":\"IOUringEvent\"").count(), 1);
+    ma::assert_ge!(
+        events.matches("\"opcode\":\"IORING_OP_EPOLL_CTL\"").count(),
+        1
+    );
+
+    //let _ = fs::remove_dir_all(bombini_temp_dir);
 }
