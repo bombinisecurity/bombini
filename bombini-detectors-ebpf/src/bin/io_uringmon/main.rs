3a1db9cded4bb31f1c9420a0b2535f41022a7f53
Andrei Fedotov <splashgitar@gmail.com>
2025-06-21 14:35:53 +0300

    feat: add process filtering for io_uringmon
    IOUringMon now supports config with process filter section

--- bombini-detectors-ebpf/src/bin/io_uringmon/main.rs
+++ bombini-detectors-ebpf/src/bin/io_uringmon/main.rs
@@ -4,18 +4,49 @@
 use aya_ebpf::{
     helpers::bpf_get_current_pid_tgid,
     macros::{btf_tracepoint, map},
-    maps::hash_map::HashMap,
+    maps::{hash_map::HashMap, lpm_trie::LpmTrie, Array},
     programs::BtfTracePointContext,
 };
+
+use bombini_common::config::io_uringmon::Config;
+use bombini_common::constants::{MAX_FILENAME_SIZE, MAX_FILE_PATH, MAX_FILE_PREFIX};
 use bombini_common::event::process::ProcInfo;
 use bombini_common::event::{Event, MSG_IOURING};
 
 use bombini_detectors_ebpf::vmlinux::io_kiocb;
 
-use bombini_detectors_ebpf::{event_capture, event_map::rb_event_init, util};
+use bombini_detectors_ebpf::{
+    event_capture, event_map::rb_event_init, filter::process::ProcessFilter, util,
+};
 
 #[map]
 static PROCMON_PROC_MAP: HashMap<u32, ProcInfo> = HashMap::pinned(1, 0);
+
+#[map]
+static IOURINGMON_CONFIG: Array<Config> = Array::with_max_entries(1, 0);
+
+// Filter maps
+
+#[map]
+static IOURINGMON_FILTER_UID_MAP: HashMap<u32, u8> = HashMap::with_max_entries(1, 0);
+
+#[map]
+static IOURINGMON_FILTER_EUID_MAP: HashMap<u32, u8> = HashMap::with_max_entries(1, 0);
+
+#[map]
+static IOURINGMON_FILTER_AUID_MAP: HashMap<u32, u8> = HashMap::with_max_entries(1, 0);
+
+#[map]
+static IOURINGMON_FILTER_BINPATH_MAP: HashMap<[u8; MAX_FILE_PATH], u8> =
+    HashMap::with_max_entries(1, 0);
+
+#[map]
+static IOURINGMON_FILTER_BINNAME_MAP: HashMap<[u8; MAX_FILENAME_SIZE], u8> =
+    HashMap::with_max_entries(1, 0);
+
+#[map]
+static IOURINGMON_FILTER_BINPREFIX_MAP: LpmTrie<[u8; MAX_FILE_PREFIX], u8> =
+    LpmTrie::with_max_entries(1, 0);
 
 #[btf_tracepoint(function = "io_uring_submit_req")]
 pub fn io_uring_submit_req_capture(ctx: BtfTracePointContext) -> u32 {
@@ -23,6 +54,13 @@ pub fn io_uring_submit_req_capture(ctx: BtfTracePointContext) -> u32 {
 }
 
 fn try_submit_req(ctx: BtfTracePointContext, event: &mut Event) -> Result<i32, i32> {
+    let Some(config_ptr) = IOURINGMON_CONFIG.get_ptr(0) else {
+        return Err(0);
+    };
+    let config = unsafe { config_ptr.as_ref() };
+    let Some(config) = config else {
+        return Err(0);
+    };
     let Event::IOUring(event) = event else {
         return Err(0);
     };
@@ -32,13 +70,37 @@ fn try_submit_req(ctx: BtfTracePointContext, event: &mut Event) -> Result<i32, i
         return Err(0);
     };
 
+    // Filter event by process
+
     unsafe {
         let req: *const io_kiocb = ctx.arg(0);
         event.opcode = (*req).opcode;
         event.flags = (*req).flags;
     }
-    util::copy_proc(proc, &mut event.process);
-    Ok(0)
+    if config.expose_events {
+        if !config.filter_mask.is_empty() {
+            let process_filter: ProcessFilter = ProcessFilter::new(
+                &IOURINGMON_FILTER_UID_MAP,
+                &IOURINGMON_FILTER_EUID_MAP,
+                &IOURINGMON_FILTER_AUID_MAP,
+                &IOURINGMON_FILTER_BINNAME_MAP,
+                &IOURINGMON_FILTER_BINPATH_MAP,
+                &IOURINGMON_FILTER_BINPREFIX_MAP,
+            );
+            let mut allow = process_filter.filter(config.filter_mask, proc);
+            if config.deny_list {
+                allow = !allow;
+            }
+            if allow {
+                util::copy_proc(proc, &mut event.process);
+                return Ok(0);
+            }
+            return Err(0);
+        }
+        util::copy_proc(proc, &mut event.process);
+        return Ok(0);
+    }
+    Err(0)
 }
 
 #[panic_handler]
