cbf84a6256a36b96525f53afde16411ceaf9639a
Andrei Fedotov <splashgitar@gmail.com>
2025-07-12 22:49:18 +0300

    tests: add test_filemon_chmod_file

--- bombini/tests/tests.rs
+++ bombini/tests/tests.rs
@@ -292,6 +292,75 @@ fn test_filemon_unlink_file() {
 }
 
 #[test]
+fn test_filemon_chmod_file() {
+    let (temp_dir, mut config, bpf_objs) = init_test_env();
+    let bombini_temp_dir = temp_dir.path();
+    let mut tmp_config = bombini_temp_dir.join("config/config.yaml");
+    let _ = fs::create_dir(bombini_temp_dir.join("config"));
+    let _ = fs::copy(&config, &tmp_config);
+    tmp_config.pop();
+    config.pop();
+    let filemon_config = tmp_config.join("filemon.yaml");
+    let _ = fs::copy(config.join("procmon.yaml"), tmp_config.join("procmon.yaml"));
+    let _ = fs::copy(config.join("filemon.yaml"), &filemon_config);
+    let bombini_log =
+        File::create(bombini_temp_dir.join("bombini.log")).expect("can't create log file");
+    let event_log = temp_dir.path().join("events.log");
+
+    let bombini = Command::new(EXE_BOMBINI)
+        .args([
+            "--config-dir",
+            tmp_config.to_str().unwrap(),
+            "--bpf-objs",
+            bpf_objs.to_str().unwrap(),
+            "--event-log",
+            event_log.to_str().unwrap(),
+            "--detector",
+            "procmon",
+            "--detector",
+            "filemon",
+        ])
+        .env("RUST_LOG", "debug")
+        .stderr(bombini_log.try_clone().unwrap())
+        .spawn();
+
+    if bombini.is_err() {
+        panic!("{:?}", bombini.err().unwrap());
+    }
+    let mut bombini = bombini.expect("failed to start bombini");
+    // Wait for detectors being loaded
+    thread::sleep(Duration::from_millis(2000));
+
+    let chmod_status = Command::new("chmod")
+        .args(["+w", filemon_config.to_str().unwrap()])
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .stdin(Stdio::null())
+        .status()
+        .expect("can't start rm");
+
+    assert!(chmod_status.success());
+
+    // Wait Events being processed
+    thread::sleep(Duration::from_millis(500));
+
+    let _ = signal::kill(Pid::from_raw(bombini.id() as i32), Signal::SIGINT);
+
+    let _ = bombini.wait().unwrap();
+
+    // TODO: more precise check
+    let events = fs::read_to_string(&event_log).expect("can't read events");
+    ma::assert_ge!(events.matches("\"type\":\"FileEvent\"").count(), 1);
+    ma::assert_ge!(events.matches("\"type\":\"PathChmod\"").count(), 1);
+    ma::assert_ge!(events.matches("\"filename\":\"chmod\"").count(), 1);
+    let mut file_path = String::from("\"path\":\"");
+    file_path.push_str(&filemon_config.to_str().unwrap());
+    assert_eq!(events.matches(&file_path).count(), 1);
+
+    let _ = fs::remove_dir_all(bombini_temp_dir);
+}
+
+#[test]
 fn test_netmon_tcp_ip4_file() {
     let (temp_dir, mut config, bpf_objs) = init_test_env();
     let bombini_temp_dir = temp_dir.path();
@@ -633,6 +702,8 @@ file_open:
 path_truncate:
   disable: true
 path_unlink:
+  disable: true
+path_chmod:
   disable: true
 process_filter:
   binary:
