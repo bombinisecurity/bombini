aae4184744e7172b93d9afca13a80c327996b03e
Andrei Fedotov <splashgitar@gmail.com>
2025-09-27 20:21:26 +0300

    refactor(gtfobins): update due to procmon changes
    Strange behavior when executing sudo <gtfobins>, eg "sudo xargs -a /dev/null sh".
    GTFObins event contains xargs PID, but filename, path, args of sudo.
    ProcessExec event of xargs is fine. Need to deal with this later.

--- bombini-detectors-ebpf/src/bin/gtfobins/main.rs
+++ bombini-detectors-ebpf/src/bin/gtfobins/main.rs
@@ -2,16 +2,13 @@
 #![no_main]
 
 use aya_ebpf::{
-    helpers::{
-        bpf_get_current_pid_tgid, bpf_probe_read_kernel, bpf_probe_read_kernel_buf,
-        bpf_probe_read_kernel_str_bytes,
-    },
+    helpers::{bpf_get_current_task_btf, bpf_probe_read_kernel, bpf_probe_read_kernel_str_bytes},
     macros::{lsm, map},
     maps::hash_map::HashMap,
     programs::LsmContext,
 };
 
-use bombini_detectors_ebpf::vmlinux::{file, linux_binprm, path, qstr};
+use bombini_detectors_ebpf::vmlinux::{file, kuid_t, linux_binprm, path, pid_t, qstr, task_struct};
 
 use bombini_common::constants::MAX_FILENAME_SIZE;
 use bombini_common::event::process::ProcInfo;
@@ -36,17 +33,16 @@ fn try_detect(ctx: LsmContext, event: &mut Event) -> Result<i32, i32> {
     let Event::GTFOBins(event) = event else {
         return Err(0);
     };
-    let pid = (bpf_get_current_pid_tgid() >> 32) as u32;
-    let proc = unsafe { PROCMON_PROC_MAP.get(&pid) };
-    let Some(mut proc) = proc else {
-        return Err(0);
-    };
+    unsafe {
+        let binprm: *const linux_binprm = ctx.arg(0);
+        let cred = (*binprm).cred;
+        let euid = bpf_probe_read_kernel::<kuid_t>(&(*cred).euid as *const _)
+            .map_err(|_| 0i32)?
+            .val;
 
-    // Check if process is privileged
-    if proc.creds.euid == 0 {
-        // Check if sh is executing
-        unsafe {
-            let binprm: *const linux_binprm = ctx.arg(0);
+        // Check if process is privileged
+        if euid == 0 {
+            // Check if sh is executing
             let file: *mut file = (*binprm).file;
             let path =
                 bpf_probe_read_kernel::<path>(&(*file).f_path as *const _).map_err(|_| 0_i32)?;
@@ -54,45 +50,57 @@ fn try_detect(ctx: LsmContext, event: &mut Event) -> Result<i32, i32> {
                 .map_err(|_| 0_i32)?;
             bpf_probe_read_kernel_str_bytes(d_name.name, &mut event.process.filename)
                 .map_err(|_| 0_i32)?;
-        }
-        if (event.process.filename[0] == b's' && event.process.filename[1] == b'h')
-            || (event.process.filename[0] == b'b'
-                && event.process.filename[1] == b'a'
-                && event.process.filename[2] == b's'
-                && event.process.filename[3] == b'h')
-            || (event.process.filename[0] == b'd'
-                && event.process.filename[1] == b'a'
-                && event.process.filename[2] == b's'
-                && event.process.filename[3] == b'h')
-            || (event.process.filename[0] == b'z'
-                && event.process.filename[1] == b's'
-                && event.process.filename[2] == b'h')
-        {
-            for _ in 0..MAX_PROC_DEPTH {
-                unsafe {
-                    let _ = bpf_probe_read_kernel_buf(
-                        proc.filename.as_ptr(),
-                        &mut event.process.filename,
-                    );
-                }
-                let parent_proc = unsafe { PROCMON_PROC_MAP.get(&proc.ppid) };
-                let Some(parent_proc) = parent_proc else {
-                    return Err(0);
-                };
-                // Check if GTFO binary
-                if let Some(enforce) = GTFOBINS_NAME_MAP.get_ptr(&event.process.filename) {
-                    if proc.clonned {
-                        // Pass parent process in event
+            if (event.process.filename[0] == b's' && event.process.filename[1] == b'h')
+                || (event.process.filename[0] == b'b'
+                    && event.process.filename[1] == b'a'
+                    && event.process.filename[2] == b's'
+                    && event.process.filename[3] == b'h')
+                || (event.process.filename[0] == b'd'
+                    && event.process.filename[1] == b'a'
+                    && event.process.filename[2] == b's'
+                    && event.process.filename[3] == b'h')
+                || (event.process.filename[0] == b'z'
+                    && event.process.filename[1] == b's'
+                    && event.process.filename[2] == b'h')
+            {
+                let task = bpf_get_current_task_btf() as *const task_struct;
+                let parent_task =
+                    bpf_probe_read_kernel::<*mut task_struct>(&(*task).parent as *const _)
+                        .map_err(|_| 0i32)?;
+                let mut ppid = bpf_probe_read_kernel(&(*parent_task).tgid as *const pid_t)
+                    .map_err(|_| 0i32)? as u32;
+                for _ in 0..MAX_PROC_DEPTH {
+                    let parent_proc = PROCMON_PROC_MAP.get(&ppid);
+                    let Some(parent_proc) = parent_proc else {
+                        return Err(0);
+                    };
+                    // Check if GTFO binary
+                    if let Some(enforce) = GTFOBINS_NAME_MAP.get_ptr(&parent_proc.filename) {
                         util::copy_proc(parent_proc, &mut event.process);
-                    } else {
-                        util::copy_proc(proc, &mut event.process);
+                        if *enforce != 0 {
+                            return Ok(-1);
+                        }
+                        return Ok(0);
                     }
-                    if unsafe { *enforce != 0 } {
-                        return Ok(-1);
+                    if parent_proc.filename[0] == b's'
+                        && parent_proc.filename[1] == b'u'
+                        && parent_proc.filename[2] == b'd'
+                        && parent_proc.filename[3] == b'o'
+                    {
+                        bpf_probe_read_kernel_str_bytes(
+                            &parent_proc.args as *const _,
+                            &mut event.process.filename,
+                        )
+                        .map_err(|_| 0_i32)?;
+                        if let Some(enforce) = GTFOBINS_NAME_MAP.get_ptr(&event.process.filename) {
+                            util::copy_proc(parent_proc, &mut event.process);
+                            if *enforce != 0 {
+                                return Ok(-1);
+                            }
+                            return Ok(0);
+                        }
                     }
-                    return Ok(0);
-                } else {
-                    proc = parent_proc;
+                    ppid = parent_proc.ppid;
                 }
             }
         }
