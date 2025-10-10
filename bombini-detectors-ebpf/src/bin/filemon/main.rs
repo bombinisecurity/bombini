72c58307771412d147eb94cd8111f4bd82fdc428
Andrei Fedotov <splashgitar@gmail.com>
2025-10-10 20:36:24 +0300

    feat(filemon): Add Path filtering initial support (file_open hook)
    Filtering FileEvents by path allow list (name OR full file path OR prefix).

--- bombini-detectors-ebpf/src/bin/filemon/main.rs
+++ bombini-detectors-ebpf/src/bin/filemon/main.rs
@@ -7,7 +7,9 @@ use aya_ebpf::{
         bpf_probe_read_kernel_str_bytes,
     },
     macros::{lsm, map},
-    maps::{array::Array, hash_map::HashMap, lpm_trie::LpmTrie, LruHashMap},
+    maps::{
+        array::Array, hash_map::HashMap, lpm_trie::LpmTrie, per_cpu_array::PerCpuArray, LruHashMap,
+    },
     programs::LsmContext,
 };
 
@@ -23,7 +25,8 @@ use bombini_common::event::{Event, MSG_FILE};
 use bombini_detectors_ebpf::vmlinux::{dentry, file, kgid_t, kuid_t, path, qstr};
 
 use bombini_detectors_ebpf::{
-    event_capture, event_map::rb_event_init, filter::process::ProcessFilter, util,
+    event_capture, event_map::rb_event_init, filter::path::PathFilter,
+    filter::process::ProcessFilter, util,
 };
 
 #[map]
@@ -31,6 +34,9 @@ static PROCMON_PROC_MAP: LruHashMap<u32, ProcInfo> = LruHashMap::pinned(1, 0);
 
 #[map]
 static FILEMON_CONFIG: Array<Config> = Array::with_max_entries(1, 0);
+
+#[map]
+static PATH_HEAP: PerCpuArray<[u8; MAX_FILE_PATH]> = PerCpuArray::with_max_entries(1, 0);
 
 // Filter maps
 
@@ -86,9 +92,21 @@ fn filter_by_process(config: &Config, proc: &ProcInfo) -> Result<(), i32> {
     Ok(())
 }
 
+#[map]
+static FILEMON_FILTER_OPEN_PATH_MAP: HashMap<[u8; MAX_FILE_PATH], u8> =
+    HashMap::with_max_entries(1, 0);
+
+#[map]
+static FILEMON_FILTER_OPEN_NAME_MAP: HashMap<[u8; MAX_FILENAME_SIZE], u8> =
+    HashMap::with_max_entries(1, 0);
+
+#[map]
+static FILEMON_FILTER_OPEN_PREFIX_MAP: LpmTrie<[u8; MAX_FILE_PREFIX], u8> =
+    LpmTrie::with_max_entries(1, 0);
+
 #[lsm(hook = "file_open")]
 pub fn file_open_capture(ctx: LsmContext) -> i32 {
-    event_capture!(ctx, MSG_FILE, false, try_open)
+    event_capture!(ctx, MSG_FILE, true, try_open)
 }
 
 fn try_open(ctx: LsmContext, event: &mut Event) -> Result<i32, i32> {
@@ -112,12 +130,17 @@ fn try_open(ctx: LsmContext, event: &mut Event) -> Result<i32, i32> {
 
     event.hook = HOOK_FILE_OPEN;
     unsafe {
+        let Some(path_ptr) = PATH_HEAP.get_ptr_mut(0) else {
+            return Err(0);
+        };
         let fp: *const file = ctx.arg(0);
         let _ = bpf_d_path(
             &(*fp).f_path as *const _ as *mut aya_ebpf::bindings::path,
-            event.path.as_mut_ptr() as *mut _,
-            event.path.len() as u32,
+            path_ptr as *mut _,
+            MAX_FILE_PATH as u32,
         );
+        bpf_probe_read_kernel_str_bytes(path_ptr as *const _, &mut event.path).map_err(|_| 0i32)?;
+
         event.flags = (*fp).f_flags;
         event.i_mode = (*(*fp).f_inode).i_mode;
         event.uid = bpf_probe_read_kernel::<kuid_t>(&(*(*fp).f_inode).i_uid as *const _)
@@ -126,6 +149,24 @@ fn try_open(ctx: LsmContext, event: &mut Event) -> Result<i32, i32> {
         event.gid = bpf_probe_read_kernel::<kgid_t>(&(*(*fp).f_inode).i_gid as *const _)
             .map_err(|_| 0i32)?
             .val;
+        // Filter event by path
+        if !config.path_mask[0].is_empty() {
+            let path_filter: PathFilter = PathFilter::new(
+                &FILEMON_FILTER_OPEN_NAME_MAP,
+                &FILEMON_FILTER_OPEN_PATH_MAP,
+                &FILEMON_FILTER_OPEN_PREFIX_MAP,
+            );
+            let path =
+                bpf_probe_read_kernel::<path>(&(*fp).f_path as *const _).map_err(|_| 0i32)?;
+
+            let d_name = bpf_probe_read_kernel::<qstr>(&(*(path.dentry)).d_name as *const _)
+                .map_err(|_| 0i32)?;
+
+            bpf_probe_read_kernel_str_bytes(d_name.name, &mut event.name).map_err(|_| 0i32)?;
+            if !path_filter.filter(config.path_mask[0], &event.path, &event.name) {
+                return Err(0);
+            }
+        }
     }
     util::copy_proc(proc, &mut event.process);
     Ok(0)
--- bombini-detectors-ebpf/src/filter/mod.rs
+++ bombini-detectors-ebpf/src/filter/mod.rs
@@ -1,3 +1,4 @@
 //! Provide interface for event filtering, support Allow lists and Deny lists.
 
+pub mod path;
 pub mod process;
