b2081d0dfe9ce6b3e1eddc939153d9dacbaa26f3
Andrei Fedotov <an.n.fedotov@t-tech.dev>
2026-02-20 11:35:08 +0300

    feat(ebpf): add port filtering in Ipv4/Ivp6 filters
    Signed-off-by: Andrei Fedotov <an.n.fedotov@t-tech.dev>

--- bombini-detectors-ebpf/src/filter/netmon/ip.rs
+++ bombini-detectors-ebpf/src/filter/netmon/ip.rs
@@ -1,5 +1,5 @@
-use aya_ebpf::maps::{LpmTrie, lpm_trie::Key};
-use bombini_common::config::rule::{ConnectionAttributes, Ipv4MapKey, Ipv6MapKey};
+use aya_ebpf::maps::{HashMap, LpmTrie, lpm_trie::Key};
+use bombini_common::config::rule::{ConnectionAttributes, Ipv4MapKey, Ipv6MapKey, PortKey};
 
 use crate::interpreter::CheckIn;
 
@@ -7,25 +7,38 @@ use crate::interpreter::CheckIn;
 pub struct Ipv4Filter<'a> {
     pub src_ip_addr_map: &'a LpmTrie<Ipv4MapKey, u8>,
     pub dst_ip_addr_map: &'a LpmTrie<Ipv4MapKey, u8>,
+    pub src_port_map: &'a HashMap<PortKey, u8>,
+    pub dst_port_map: &'a HashMap<PortKey, u8>,
 
     pub src_ip_addr: &'a Key<Ipv4MapKey>,
     pub dst_ip_addr: &'a Key<Ipv4MapKey>,
+    pub src_port: &'a PortKey,
+    pub dst_port: &'a PortKey,
 }
 
 impl<'a> Ipv4Filter<'a> {
+    #[allow(clippy::too_many_arguments)]
     pub fn new(
         src_ip_addr_map: &'a LpmTrie<Ipv4MapKey, u8>,
         dst_ip_addr_map: &'a LpmTrie<Ipv4MapKey, u8>,
+        src_port_map: &'a HashMap<PortKey, u8>,
+        dst_port_map: &'a HashMap<PortKey, u8>,
 
         src_ip_addr: &'a Key<Ipv4MapKey>,
         dst_ip_addr: &'a Key<Ipv4MapKey>,
+        src_port: &'a PortKey,
+        dst_port: &'a PortKey,
     ) -> Self {
         Self {
             src_ip_addr_map,
             dst_ip_addr_map,
+            src_port_map,
+            dst_port_map,
 
             src_ip_addr,
             dst_ip_addr,
+            src_port,
+            dst_port,
         }
     }
 }
@@ -51,6 +64,18 @@ impl CheckIn for Ipv4Filter<'_> {
                 // lookups for IPv6 are not possible while processing IPv4
                 Ok(false)
             }
+            id if id == ConnectionAttributes::PortSrc as u8 => unsafe {
+                let Some(mask_port) = self.src_port_map.get(self.src_port) else {
+                    return Ok(false);
+                };
+                Ok(*mask_port & (1 << in_op_idx) != 0)
+            },
+            id if id == ConnectionAttributes::PortDst as u8 => unsafe {
+                let Some(mask_port) = self.dst_port_map.get(self.dst_port) else {
+                    return Ok(false);
+                };
+                Ok(*mask_port & (1 << in_op_idx) != 0)
+            },
             _ => Err(0),
         }
     }
@@ -60,25 +85,38 @@ impl CheckIn for Ipv4Filter<'_> {
 pub struct Ipv6Filter<'a> {
     pub src_ip_addr_map: &'a LpmTrie<Ipv6MapKey, u8>,
     pub dst_ip_addr_map: &'a LpmTrie<Ipv6MapKey, u8>,
+    pub src_port_map: &'a HashMap<PortKey, u8>,
+    pub dst_port_map: &'a HashMap<PortKey, u8>,
 
     pub src_ip_addr: &'a Key<Ipv6MapKey>,
     pub dst_ip_addr: &'a Key<Ipv6MapKey>,
+    pub src_port: &'a PortKey,
+    pub dst_port: &'a PortKey,
 }
 
 impl<'a> Ipv6Filter<'a> {
+    #[allow(clippy::too_many_arguments)]
     pub fn new(
         src_ip_addr_map: &'a LpmTrie<Ipv6MapKey, u8>,
         dst_ip_addr_map: &'a LpmTrie<Ipv6MapKey, u8>,
+        src_port_map: &'a HashMap<PortKey, u8>,
+        dst_port_map: &'a HashMap<PortKey, u8>,
 
         src_ip_addr: &'a Key<Ipv6MapKey>,
         dst_ip_addr: &'a Key<Ipv6MapKey>,
+        src_port: &'a PortKey,
+        dst_port: &'a PortKey,
     ) -> Self {
         Self {
             src_ip_addr_map,
             dst_ip_addr_map,
+            src_port_map,
+            dst_port_map,
 
             src_ip_addr,
             dst_ip_addr,
+            src_port,
+            dst_port,
         }
     }
 }
@@ -104,6 +142,18 @@ impl CheckIn for Ipv6Filter<'_> {
                 // lookups for IPv4 are not possible while processing IPv6
                 Ok(false)
             }
+            id if id == ConnectionAttributes::PortSrc as u8 => unsafe {
+                let Some(mask_port) = self.src_port_map.get(self.src_port) else {
+                    return Ok(false);
+                };
+                Ok(*mask_port & (1 << in_op_idx) != 0)
+            },
+            id if id == ConnectionAttributes::PortDst as u8 => unsafe {
+                let Some(mask_port) = self.dst_port_map.get(self.dst_port) else {
+                    return Ok(false);
+                };
+                Ok(*mask_port & (1 << in_op_idx) != 0)
+            },
             _ => Err(0),
         }
     }
