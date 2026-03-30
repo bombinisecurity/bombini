6d8f50f596971688e71a9fb45c375e5fc73a7ca6
Kivooeo <Kivooeo123@gmail.com>
2026-03-31 02:09:35 +0300

    create co_re.rs
    Signed-off-by: Kivooeo <Kivooeo123@gmail.com>

---
+++ bombini-detectors-ebpf/src/co_re/co_re.rs
@@ -0,0 +1,121 @@
+#[allow(non_camel_case_types)]
+#[allow(non_upper_case_globals)]
+#[allow(dead_code)]
+mod r#gen;
+
+mod core_task_struct;
+pub use core_task_struct::*;
+
+mod core_cred;
+pub use core_cred::*;
+
+mod core_mm_struct;
+pub use core_mm_struct::*;
+
+mod core_fs;
+pub use core_fs::*;
+
+mod core_exec;
+pub use core_exec::*;
+
+mod core_sock;
+pub use core_sock::*;
+
+mod core_cgroup;
+pub use core_cgroup::*;
+
+mod core_kernfs;
+pub use core_kernfs::*;
+
+mod core_io_uring;
+pub use core_io_uring::*;
+
+#[derive(Clone, Copy)]
+pub struct CoRe<P> {
+    ptr: *const P,
+}
+
+impl<P> PartialEq for CoRe<P> {
+    fn eq(&self, other: &Self) -> bool {
+        self.ptr == other.ptr
+    }
+}
+
+impl<P> From<*mut P> for CoRe<P> {
+    fn from(value: *mut P) -> Self {
+        Self::from_ptr(value)
+    }
+}
+
+impl<P> From<*const P> for CoRe<P> {
+    fn from(value: *const P) -> Self {
+        Self::from_ptr(value)
+    }
+}
+
+impl<P> CoRe<P> {
+    #[inline(always)]
+    pub unsafe fn bpf_read(&self) -> Result<*const P, i64> {
+        aya_ebpf::helpers::bpf_probe_read(&self.ptr)
+    }
+
+    #[inline(always)]
+    pub fn is_null(&self) -> bool {
+        self.ptr.is_null()
+    }
+
+    pub fn as_ptr(&self) -> *const P {
+        self.ptr as *mut _
+    }
+
+    fn as_ptr_mut(&self) -> *mut P {
+        self.ptr as *mut _
+    }
+
+    pub fn from_ptr(ptr: *const P) -> Self {
+        CoRe {
+            ptr: ptr as *const _,
+        }
+    }
+}
+
+macro_rules! rust_shim_kernel_impl {
+    ($struct:ident, $member:ident, $ret:ty) => {
+        rust_shim_kernel_impl! (pub, $member, $struct, $member, $ret);
+    };
+
+    ($pub:vis, $struct:ident, $member:ident, $ret:ty) => {
+        rust_shim_kernel_impl! ($pub, $member, $struct, $member, $ret);
+    };
+
+    ($pub:vis, $fn_name:ident, $struct: ident, $member:ident, $ret:ty) => {
+        #[inline(always)]
+        $pub unsafe fn $fn_name(&self) -> Option<$ret> {
+            if !self.is_null()
+                && paste::paste! {[<shim_ $struct _ $member _exists>]}(self.as_ptr_mut())
+            {
+                return Some(paste::paste! {[<shim_ $struct _ $member>]}(self.as_ptr_mut()).into());
+            }
+            None
+        }
+    };
+}
+
+pub(crate) use rust_shim_kernel_impl;
+
+macro_rules! core_read_kernel {
+    ($struc:expr, $field:ident) => {
+        $struc
+            .$field()
+    };
+
+    ($struc:expr, $first:ident, $($rest: ident),*) => {
+        $struc
+            .$first()
+            $(
+            .and_then(|r| r.$rest())
+            )*
+    };
+}
+
+pub(crate) use core_read_kernel;
