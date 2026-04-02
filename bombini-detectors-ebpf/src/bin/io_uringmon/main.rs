0c133742f1155c2868cc3c10ffaabb06d49822e9
Kivooeo <Kivooeo123@gmail.com>
2026-04-03 10:09:37 +1100

    refactor io_uringmon
    Signed-off-by: Kivooeo <Kivooeo123@gmail.com>

--- bombini-detectors-ebpf/src/bin/io_uringmon/main.rs
+++ bombini-detectors-ebpf/src/bin/io_uringmon/main.rs
@@ -13,11 +13,11 @@ use aya_ebpf::{
     },
     programs::BtfTracePointContext,
 };
-
 use bombini_common::constants::{MAX_FILE_PATH, MAX_FILE_PREFIX, MAX_FILENAME_SIZE};
 use bombini_common::event::io_uring::IOUringOp;
 use bombini_common::event::process::ProcInfo;
 use bombini_common::event::{Event, GenericEvent, MSG_IOURING};
+use bombini_detectors_ebpf::co_re::{self, core_read_kernel};
 
 use bombini_detectors_ebpf::vmlinux::{file, filename, io_kiocb, open_how, sockaddr};
 
@@ -67,29 +67,30 @@ fn try_submit_req(ctx: BtfTracePointContext, generic_event: &mut GenericEvent) -
     // Filter event by process
     unsafe {
         let req: *const io_kiocb = ctx.arg(0);
-        event.opcode = core::mem::transmute::<u8, IOUringOp>((*req).opcode);
+        let req_core = co_re::io_kiocb::from_ptr(req as *const _);
+        let opcode = core_read_kernel!(req_core, opcode).ok_or(0i32)?;
+        event.opcode = core::mem::transmute::<u8, IOUringOp>(opcode);
         match event.opcode {
             IOUringOp::IORING_OP_OPENAT | IOUringOp::IORING_OP_OPENAT2 => {
                 let open_data = bpf_probe_read_kernel::<io_open>(
                     &(*req).__bindgen_anon_1.cmd as *const _ as *const _,
                 )
                 .map_err(|_| 0i32)?;
-                let filename_data =
-                    bpf_probe_read_kernel::<filename>(open_data.filename as *const _)
-                        .map_err(|_| 0i32)?;
-                bpf_probe_read_kernel_str_bytes(filename_data.name as *const _, &mut event.path)
+                let filename = co_re::filename::from_ptr(open_data.filename as *const _);
+                let name = core_read_kernel!(filename, name).ok_or(0i32)?;
+                bpf_probe_read_kernel_str_bytes(name as *const u8, &mut event.path)
                     .map_err(|_| 0i32)?;
-                event.flags = open_data.how.flags;
+                let how_core = co_re::open_how::from_ptr(&open_data.how as *const _ as *const _);
+                event.flags = core_read_kernel!(how_core, flags).unwrap_or(0);
             }
             IOUringOp::IORING_OP_STATX => {
                 let statx_data = bpf_probe_read_kernel::<io_statx>(
                     &(*req).__bindgen_anon_1.cmd as *const _ as *const _,
                 )
                 .map_err(|_| 0i32)?;
-                let filename_data =
-                    bpf_probe_read_kernel::<filename>(statx_data.filename as *const _)
-                        .map_err(|_| 0i32)?;
-                bpf_probe_read_kernel_str_bytes(filename_data.name as *const _, &mut event.path)
+                let filename = co_re::filename::from_ptr(statx_data.filename as *const _);
+                let name = core_read_kernel!(filename, name).ok_or(0i32)?;
+                bpf_probe_read_kernel_str_bytes(name as *const u8, &mut event.path)
                     .map_err(|_| 0i32)?;
             }
             IOUringOp::IORING_OP_UNLINKAT => {
@@ -97,10 +98,9 @@ fn try_submit_req(ctx: BtfTracePointContext, generic_event: &mut GenericEvent) -
                     &(*req).__bindgen_anon_1.cmd as *const _ as *const _,
                 )
                 .map_err(|_| 0i32)?;
-                let filename_data =
-                    bpf_probe_read_kernel::<filename>(unlink_data.filename as *const _)
-                        .map_err(|_| 0i32)?;
-                bpf_probe_read_kernel_str_bytes(filename_data.name as *const _, &mut event.path)
+                let filename = co_re::filename::from_ptr(unlink_data.filename as *const _);
+                let name = core_read_kernel!(filename, name).ok_or(0i32)?;
+                bpf_probe_read_kernel_str_bytes(name as *const u8, &mut event.path)
                     .map_err(|_| 0i32)?;
             }
             IOUringOp::IORING_OP_CONNECT => {
