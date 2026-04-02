75af6324ae1b8d55b39f14da97745c653819edfe
Kivooeo <Kivooeo123@gmail.com>
2026-04-03 09:17:16 +1100

    add trusted shim
    Signed-off-by: Kivooeo <Kivooeo123@gmail.com>

--- bombini-detectors-ebpf/src/co_re/core_exec.rs
+++ bombini-detectors-ebpf/src/co_re/core_exec.rs
@@ -1,3 +1,5 @@
+use crate::co_re::rust_shim_kernel_trusted_impl;
+
 use super::r#gen::{self, *};
 use super::{CoRe, cred, file, rust_shim_kernel_impl};
 
@@ -5,6 +7,7 @@ use super::{CoRe, cred, file, rust_shim_kernel_impl};
 pub type linux_binprm = CoRe<r#gen::linux_binprm>;
 
 impl linux_binprm {
+    rust_shim_kernel_trusted_impl!(pub, file_trusted, linux_binprm, file, file);
     rust_shim_kernel_impl!(pub, linux_binprm, file, file);
     rust_shim_kernel_impl!(pub, linux_binprm, cred, cred);
     rust_shim_kernel_impl!(pub, linux_binprm, per_clear, u32);
