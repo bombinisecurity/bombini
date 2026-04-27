4e960d5d7398c18bea98fa364544b97575ff6a0e
Kivooeo <Kivooeo123@gmail.com>
2026-04-27 04:21:12 +0000

    remove vmlinux
    Signed-off-by: Kivooeo <Kivooeo123@gmail.com>

--- bombini-detectors-ebpf/src/lib.rs
+++ bombini-detectors-ebpf/src/lib.rs
@@ -1,16 +1,12 @@
 #![no_std]
 #![no_main]
 
+#[allow(unsafe_op_in_unsafe_fn)]
+#[allow(clippy::missing_safety_doc)]
+pub mod co_re;
 pub mod dyn_ringbuf;
 pub mod event_map;
 pub mod filter;
 pub mod interpreter;
-pub mod util;
-#[allow(clippy::all)]
-#[allow(warnings)]
-#[rustfmt::skip]
-pub mod vmlinux;
-#[allow(unsafe_op_in_unsafe_fn)]
-#[allow(clippy::missing_safety_doc)]
-pub mod co_re;
 pub mod uapi;
+pub mod util;
